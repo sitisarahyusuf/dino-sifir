2562ca4e5c211885c41764439f9b07ff46cbf589
sitisarahyusuf <g-56076218@moe-dl.edu.my>
2026-09-06 16:25:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='dino-sifir-v1';
-const ASSETS=['./','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png'];
+const ASSETS=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{
